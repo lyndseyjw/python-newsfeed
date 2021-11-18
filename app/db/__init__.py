0a4bd23abfc9d1354ec1b37300fae0da8ccae8e2
lyndseyjw <lyndseyjwatson@gmail.com>
2021-11-18 15:11:45 -0800

    created database along with models, seeds, and all relationships

---
+++ app/db/__init__.py
@@ -0,0 +1,14 @@
+from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# connect to database using env variable
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# generates temp connections for performing CRUD operations
+Session = sessionmaker(bind=engine)
+# map the models to real MySQL tables
+Base = declarative_base()
